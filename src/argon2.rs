f71fe3c4f966ff819771303804ddd9c543dd9d13
WingZer0o <mtmulch0191@outlook.com>
2023-12-10 00:16:31 -0500

    argon2 test cases

--- src/argon2.rs
+++ src/argon2.rs
@@ -32,6 +32,21 @@ pub extern "C" fn argon2_verify(hashed_pass: *const c_char, password: *const c_c
     return result;
 }
 
+#[test]
+fn argon2_verify_test() {
+    let password = "PasswordToVerify";
+    let password_cstr = CString::new(password).unwrap();
+    let password_bytes = password_cstr.as_bytes_with_nul();
+    let password_ptr = password_bytes.as_ptr() as *const i8;
+    let hashed_password = argon2_hash(password_ptr);
+    let hashed_password_ctr = unsafe { CString::from_raw(hashed_password) };
+    let hashed_password_bytes = hashed_password_ctr.as_bytes_with_nul();
+    let hashed_password_ptr = hashed_password_bytes.as_ptr() as *const i8;
+    let is_valid = argon2_verify(hashed_password_ptr, password_ptr);
+    assert_eq!(true, is_valid);
+}
+
+
 #[no_mangle]
 pub extern "C" fn argon2_verify_thread(
     hashed_pass: *const c_char,
@@ -63,7 +78,7 @@ pub extern "C" fn argon2_verify_thread(
 }
 
 #[test]
-fn argon2_verify_test() {
+fn argon2_verify_thread_test() {
     let password = "PasswordToVerify";
     let password_cstr = CString::new(password).unwrap();
     let password_bytes = password_cstr.as_bytes_with_nul();
@@ -72,7 +87,7 @@ fn argon2_verify_test() {
     let hashed_password_ctr = unsafe { CString::from_raw(hashed_password) };
     let hashed_password_bytes = hashed_password_ctr.as_bytes_with_nul();
     let hashed_password_ptr = hashed_password_bytes.as_ptr() as *const i8;
-    let is_valid = argon2_verify(hashed_password_ptr, password_ptr);
+    let is_valid = argon2_verify_thread(hashed_password_ptr, password_ptr);
     assert_eq!(true, is_valid);
 }
 
@@ -92,6 +107,19 @@ pub extern "C" fn argon2_hash(pass_to_hash: *const c_char) -> *mut c_char {
         .into_raw();
     return password_hash;
 }
+
+#[test]
+fn argon2_hash_test() {
+    let password = "DontUseThisPassword";
+    let password_cstr = CString::new(password).unwrap();
+    let password_bytes = password_cstr.as_bytes_with_nul();
+    let password_ptr = password_bytes.as_ptr() as *const i8;
+    let hashed_password_ptr = argon2_hash(password_ptr);
+    let hashed_password_ctr = unsafe { CString::from_raw(hashed_password_ptr) };
+    let hashed_password_str = hashed_password_ctr.to_str().unwrap();
+    assert_ne!(password, hashed_password_str);
+}
+
 
 #[no_mangle]
 pub extern "C" fn argon2_hash_thread(pass_to_hash: *const c_char) -> *mut c_char {
@@ -113,12 +141,12 @@ pub extern "C" fn argon2_hash_thread(pass_to_hash: *const c_char) -> *mut c_char
 }
 
 #[test]
-fn argon2_hash_test() {
+fn argon2_hash_thread_test() {
     let password = "DontUseThisPassword";
     let password_cstr = CString::new(password).unwrap();
     let password_bytes = password_cstr.as_bytes_with_nul();
     let password_ptr = password_bytes.as_ptr() as *const i8;
-    let hashed_password_ptr = argon2_hash(password_ptr);
+    let hashed_password_ptr = argon2_hash_thread(password_ptr);
     let hashed_password_ctr = unsafe { CString::from_raw(hashed_password_ptr) };
     let hashed_password_str = hashed_password_ctr.to_str().unwrap();
     assert_ne!(password, hashed_password_str);
