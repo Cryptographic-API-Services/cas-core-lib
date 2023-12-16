b4902b8fcb77f39c1710ba22baea4b196a5de508
WingZer0o <mtmulch0191@outlook.com>
2023-12-16 12:48:48 -0500

    #2 hmac tests

--- src/hmac.rs
+++ src/hmac.rs
@@ -27,6 +27,17 @@ pub extern "C" fn hmac_sign(key: *const c_char, message: *const c_char) -> *mut 
     return CString::new(base64::encode(result)).unwrap().into_raw();
 }
 
+#[test]
+fn hmac_sign_test() {
+    let data_to_sign = "Bad Hmac Test";
+    let data_to_sign_ptr = CString::new(data_to_sign).unwrap().into_raw();
+    let hmac_key = "ThisIsAHmacKeyBadNewsBears";
+    let hmac_key_ptr = CString::new(hmac_key).unwrap().into_raw();
+    let result = hmac_sign(hmac_key_ptr, data_to_sign_ptr);
+    let result_string = unsafe { CStr::from_ptr(result) }.to_str().unwrap();
+    assert_ne!(result_string, data_to_sign);
+}
+
 #[no_mangle]
 pub extern "C" fn hmac_verify(
     key: *const c_char,
@@ -60,4 +71,15 @@ pub extern "C" fn hmac_verify(
     let mut mac = HmacSha256::new_from_slice(key_bytes).unwrap();
     mac.update(message_bytes);
     return mac.verify_slice(decoded_signature_slice).is_ok();
+}
+
+#[test]
+fn hmac_verify_test() {
+    let data_to_sign = "Bad Hmac Verify Test";
+    let data_to_sign_ptr = CString::new(data_to_sign).unwrap().into_raw();
+    let hmac_key = "BadNewsBearsSomeHmacKey";
+    let hmac_key_ptr = CString::new(hmac_key).unwrap().into_raw();
+    let result = hmac_sign(hmac_key_ptr, data_to_sign_ptr);
+    let verify_result = hmac_verify(hmac_key_ptr, data_to_sign_ptr, result);
+    assert_eq!(true, verify_result);
 }
