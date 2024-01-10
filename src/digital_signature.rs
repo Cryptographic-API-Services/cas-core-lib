73a1bf45631e74b433be5e752ea50275a2bf0241
Mike Mulchrone <mtmulch0191@outlook.com>
2024-01-09 19:42:14 -0500

    #28 digital signature SHA512 RSA

--- src/digital_signature.rs
+++ src/digital_signature.rs
@@ -1,7 +1,7 @@
 
-use std::ffi::{c_char, c_uchar, CString};
+use std::ffi::{c_char, c_uchar, CString, CStr};
 
-use rsa::{RsaPrivateKey, RsaPublicKey, PaddingScheme, rand_core::OsRng, pkcs1::EncodeRsaPublicKey, pkcs8::EncodePrivateKey};
+use rsa::{RsaPrivateKey, RsaPublicKey, PaddingScheme, rand_core::OsRng, pkcs1::{EncodeRsaPublicKey, DecodeRsaPublicKey}, pkcs8::EncodePrivateKey, PublicKey};
 use sha3::{Digest, Sha3_256, Sha3_512};
 
 #[repr(C)]
@@ -36,4 +36,40 @@ pub extern "C" fn sha_512_rsa_digital_signature(rsa_key_size: usize, data_to_sig
     };
     std::mem::forget(signed_data);
     result
+}
+
+#[no_mangle]
+pub extern "C" fn sha_512_rsa_digital_signature_verify(
+    public_key: *const c_char, 
+    data_to_verify: *const c_uchar,
+    data_to_verify_length: usize,
+    signature: *const c_uchar,
+    signature_length: usize
+) -> bool {
+    let public_key_string = unsafe {
+        assert!(!public_key.is_null());
+        CStr::from_ptr(public_key)
+    }.to_str().unwrap();
+    let data_to_verify_slice: &[u8] = unsafe {
+        assert!(!data_to_verify.is_null());
+        std::slice::from_raw_parts(data_to_verify, data_to_verify_length)
+    };
+    let signature_slice: &[u8] = unsafe {
+        assert!(!signature.is_null());
+        std::slice::from_raw_parts(signature, signature_length)
+    };
+    let mut hasher = Sha3_512::new();
+    hasher.update(data_to_verify_slice);
+    let sha_hasher_result = hasher.finalize();
+    let public_key = RsaPublicKey::from_pkcs1_pem(public_key_string).unwrap();
+    let verified = public_key.verify(
+        PaddingScheme::new_pkcs1v15_sign_raw(),
+        &sha_hasher_result,
+        &signature_slice,
+    );
+    if verified.is_err() == false {
+        return true;
+    } else {
+        return false;
+    }
 }
