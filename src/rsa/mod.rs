03c835247292db50d098184666bebecb14d3c239
Mike Mulchrone <mtmulch0191@outlook.com>
2024-05-19 11:04:34 -0400

    removing imports and useless tests

--- src/rsa/mod.rs
+++ src/rsa/mod.rs
@@ -1,19 +1,13 @@
 use std::ffi::{c_char, c_uchar, CStr, CString};
-use std::sync::mpsc::{self, Receiver};
 
 use cas_lib::asymmetric::cas_asymmetric_encryption::CASRSAEncryption;
 use cas_lib::asymmetric::cas_rsa::CASRSA;
 use cas_lib::asymmetric::types::RSAKeyPairResult;
 use rand::rngs::OsRng;
-use rsa::RsaPrivateKey;
-use rsa::{
-    pkcs1::{DecodeRsaPublicKey, EncodeRsaPublicKey},
-    pkcs8::{DecodePrivateKey, EncodePrivateKey},
-    PaddingScheme, PublicKey, RsaPublicKey,
-};
+use rsa::{RsaPrivateKey, RsaPublicKey};
 
 mod types;
-use self::types::{RsaDecryptBytesResult, RsaEncryptBytesResult, RsaKeyGenerationThreadPool, RsaKeyPair, RsaSignBytesResults};
+use self::types::{RsaDecryptBytesResult, RsaEncryptBytesResult, RsaKeyPair, RsaSignBytesResults};
 
 #[no_mangle]
 pub extern "C" fn rsa_encrypt_bytes(
@@ -26,20 +20,14 @@ pub extern "C" fn rsa_encrypt_bytes(
         CStr::from_ptr(pub_key)
     }
     .to_str()
-    .unwrap();
+    .unwrap()
+    .to_string();
+
     let data_to_encrypt_slice = unsafe {
         assert!(!data_to_encrypt.is_null());
         std::slice::from_raw_parts(data_to_encrypt, data_to_encrypt_length)
-    };
-    let public_key = RsaPublicKey::from_pkcs1_pem(pub_key_string).unwrap();
-    let mut rng = rand::thread_rng();
-    let mut encrypted_bytes = public_key
-        .encrypt(
-            &mut rng,
-            PaddingScheme::new_pkcs1v15_encrypt(),
-            data_to_encrypt_slice,
-        )
-        .unwrap();
+    }.to_vec();
+    let mut encrypted_bytes = <CASRSA as CASRSAEncryption>::encrypt_plaintext(pub_key_string, data_to_encrypt_slice);
     let capacity = encrypted_bytes.capacity();
     encrypted_bytes.reserve_exact(capacity);
     let result = RsaEncryptBytesResult {
@@ -92,20 +80,15 @@ pub extern "C" fn rsa_decrypt_bytes(
         CStr::from_ptr(priv_key)
     }
     .to_str()
-    .unwrap();
+    .unwrap()
+    .to_string();
 
-    let data_to_decrypt_slice: &[u8] = unsafe {
+    let data_to_decrypt_slice = unsafe {
         assert!(!data_to_decrypt.is_null());
         std::slice::from_raw_parts(data_to_decrypt, data_to_decrypt_length)
-    };
+    }.to_vec();
 
-    let private_key = RsaPrivateKey::from_pkcs8_pem(priv_key_string).unwrap();
-    let mut decrypted_bytes = private_key
-        .decrypt(
-            PaddingScheme::new_pkcs1v15_encrypt(),
-            &data_to_decrypt_slice,
-        )
-        .expect("failed to decrypt");
+    let mut decrypted_bytes = <CASRSA as CASRSAEncryption>::decrypt_ciphertext(priv_key_string, data_to_decrypt_slice);
     let capacity = decrypted_bytes.capacity();
     decrypted_bytes.reserve_exact(capacity);
     let result = RsaDecryptBytesResult {
@@ -147,29 +130,12 @@ pub extern "C" fn rsa_decrypt_bytes_threadpool(
 
 #[no_mangle]
 pub extern "C" fn get_key_pair(key_size: usize) -> RsaKeyPair {
-    let mut rng: OsRng = OsRng;
-    let private_key: RsaPrivateKey =
-        RsaPrivateKey::new(&mut rng, key_size).expect("failed to generate a key");
-    let public_key: RsaPublicKey = private_key.to_public_key();
-    let key_pair = RsaKeyPair {
-        pub_key: CString::new(
-            public_key
-                .to_pkcs1_pem(rsa::pkcs1::LineEnding::LF)
-                .unwrap()
-                .to_string(),
-        )
-        .unwrap()
-        .into_raw(),
-        priv_key: CString::new(
-            private_key
-                .to_pkcs8_pem(rsa::pkcs8::LineEnding::LF)
-                .unwrap()
-                .to_string(),
-        )
-        .unwrap()
-        .into_raw(),
+    let key_pair = <CASRSA as CASRSAEncryption>::generate_rsa_keys(key_size);
+    let result = RsaKeyPair {
+        priv_key: CString::new(key_pair.private_key).unwrap().into_raw(),
+        pub_key: CString::new(key_pair.public_key).unwrap().into_raw()
     };
-    return key_pair;
+    result
 }
 
 #[no_mangle]
@@ -194,16 +160,14 @@ pub extern "C" fn rsa_sign_with_key_bytes(
         CStr::from_ptr(private_key)
     }
     .to_str()
-    .unwrap();
-    let data_to_sign_slice: &[u8] = unsafe {
+    .unwrap()
+    .to_string();
+
+    let data_to_sign_slice = unsafe {
         assert!(!data_to_sign.is_null());
         std::slice::from_raw_parts(data_to_sign, data_to_sign_length)
-    };
-    let private_key =
-        RsaPrivateKey::from_pkcs8_pem(private_key_string).expect("failed to generate a key");
-    let mut signed_data = private_key
-        .sign(PaddingScheme::new_pkcs1v15_sign_raw(), data_to_sign_slice)
-        .unwrap();
+    }.to_vec();
+    let mut signed_data = <CASRSA as CASRSAEncryption>::sign(private_key_string, data_to_sign_slice);
     let capacity = signed_data.capacity();
     signed_data.reserve_exact(capacity);
     let result = RsaSignBytesResults {
@@ -245,19 +209,6 @@ pub extern "C" fn rsa_sign_with_key_bytes_threadpool(
     return result;
 }
 
-#[test]
-fn rsa_sign_nonffi_test() {
-    let mut rng: OsRng = OsRng;
-    let private_key: RsaPrivateKey =
-        RsaPrivateKey::new(&mut rng, 2094).expect("failed to generate a key");
-    let public_key: RsaPublicKey = private_key.to_public_key();
-    let data = b"testing";
-    let signature = private_key
-        .sign(PaddingScheme::new_pkcs1v15_sign_raw(), data)
-        .unwrap();
-    assert_ne!(data.as_slice(), signature);
-}
-
 #[no_mangle]
 pub extern "C" fn rsa_verify_bytes(
     public_key: *const c_char,
@@ -272,26 +223,20 @@ pub extern "C" fn rsa_verify_bytes(
         CStr::from_ptr(public_key)
     }
     .to_str()
-    .unwrap();
-    let data_to_verify_slice: &[u8] = unsafe {
+    .unwrap()
+    .to_string();
+
+    let data_to_verify_slice = unsafe {
         assert!(!data_to_verify.is_null());
         std::slice::from_raw_parts(data_to_verify, data_to_verify_length)
-    };
-    let signature_slice: &[u8] = unsafe {
+    }.to_vec();
+
+    let signature_slice = unsafe {
         assert!(!signature.is_null());
         std::slice::from_raw_parts(signature, signature_length)
-    };
-    let public_key = RsaPublicKey::from_pkcs1_pem(public_key_string).unwrap();
-    let verified = public_key.verify(
-        PaddingScheme::new_pkcs1v15_sign_raw(),
-        &data_to_verify_slice,
-        &signature_slice,
-    );
-    if verified.is_err() == false {
-        return true;
-    } else {
-        return false;
-    }
+    }.to_vec();
+    let verified = <CASRSA as CASRSAEncryption>::verify(public_key_string, data_to_verify_slice, signature_slice);
+    verified
 }
 
 #[no_mangle]
@@ -326,20 +271,6 @@ pub extern "C" fn rsa_verify_bytes_threadpool(
     } else {
         return false;
     }
-}
-
-#[test]
-fn rsa_verify_nonffi_test() {
-    let mut rng: OsRng = OsRng;
-    let private_key: RsaPrivateKey =
-        RsaPrivateKey::new(&mut rng, 2094).expect("failed to generate a key");
-    let public_key: RsaPublicKey = private_key.to_public_key();
-    let data = "testing".as_bytes();
-    let signature = private_key
-        .sign(PaddingScheme::new_pkcs1v15_sign_raw(), data)
-        .unwrap();
-    let verified = public_key.verify(PaddingScheme::new_pkcs1v15_sign_raw(), &data, &signature);
-    assert_eq!(verified.is_err(), false);
 }
 
 #[test]
