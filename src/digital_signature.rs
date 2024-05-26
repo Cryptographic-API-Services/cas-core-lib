5b8e2afe9981848d3bcae6b45c053435449376e8
Mike Mulchrone <mtmulch0191@outlook.com>
2024-05-25 20:58:52 -0400

    rsa digital signature to cas-lib

--- src/digital_signature.rs
+++ src/digital_signature.rs
@@ -1,12 +1,7 @@
 use std::ffi::{c_char, c_uchar, CStr, CString};
 
+use cas_lib::digital_signature::{cas_digital_signature_rsa::RSADigitalSignature, sha_256_rsa::SHA256RSADigitalSignature, sha_512_rsa::SHA512RSADigitalSignature};
 use ed25519_dalek::{Keypair, Signature, Signer, Verifier};
-use rsa::{
-    pkcs1::{DecodeRsaPublicKey, EncodeRsaPublicKey},
-    pkcs8::EncodePrivateKey,
-    rand_core::OsRng,
-    PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey,
-};
 use sha3::{Digest, Sha3_256, Sha3_512};
 
 #[repr(C)]
@@ -32,39 +27,17 @@ pub extern "C" fn sha_512_rsa_digital_signature(
     data_length: usize,
 ) -> SHARSADigitalSignatureResult {
     assert!(!data_to_sign.is_null());
-    let data_to_sign_slice = unsafe { std::slice::from_raw_parts(data_to_sign, data_length) };
+    let data_to_sign_slice = unsafe { std::slice::from_raw_parts(data_to_sign, data_length) }.to_vec();
     if rsa_key_size != 1024 && rsa_key_size != 2048 && rsa_key_size != 4096 {
         panic!("Not a valid RSA key length");
     }
-    let mut hasher = Sha3_512::new();
-    hasher.update(data_to_sign_slice);
-    let sha_hasher_result = hasher.finalize();
-    let mut rng: OsRng = OsRng;
-    let private_key: RsaPrivateKey =
-        RsaPrivateKey::new(&mut rng, rsa_key_size).expect("failed to generate a key");
-    let public_key: RsaPublicKey = private_key.to_public_key();
-    let mut signed_data = private_key
-        .sign(PaddingScheme::new_pkcs1v15_sign_raw(), &sha_hasher_result)
-        .unwrap();
+    let result = <SHA512RSADigitalSignature as RSADigitalSignature>::digital_signature_rsa(rsa_key_size as u32, data_to_sign_slice);
+    let mut signed_data = result.signature;
     let capacity = signed_data.capacity();
     signed_data.reserve_exact(capacity);
     let result = SHARSADigitalSignatureResult {
-        public_key: CString::new(
-            public_key
-                .to_pkcs1_pem(rsa::pkcs8::LineEnding::LF)
-                .unwrap()
-                .to_string(),
-        )
-        .unwrap()
-        .into_raw(),
-        private_key: CString::new(
-            private_key
-                .to_pkcs8_pem(rsa::pkcs8::LineEnding::LF)
-                .unwrap()
-                .to_string(),
-        )
-        .unwrap()
-        .into_raw(),
+        public_key: CString::new(result.public_key).unwrap().into_raw(),
+        private_key: CString::new(result.private_key).unwrap().into_raw(),
         signature_raw_ptr: signed_data.as_mut_ptr(),
         length: signed_data.len(),
     };
@@ -79,39 +52,17 @@ pub extern "C" fn sha_256_rsa_digital_signature(
     data_length: usize,
 ) -> SHARSADigitalSignatureResult {
     assert!(!data_to_sign.is_null());
-    let data_to_sign_slice = unsafe { std::slice::from_raw_parts(data_to_sign, data_length) };
+    let data_to_sign_slice = unsafe { std::slice::from_raw_parts(data_to_sign, data_length) }.to_vec();
     if rsa_key_size != 1024 && rsa_key_size != 2048 && rsa_key_size != 4096 {
         panic!("Not a valid RSA key length");
     }
-    let mut hasher = Sha3_256::new();
-    hasher.update(data_to_sign_slice);
-    let sha_hasher_result = hasher.finalize();
-    let mut rng: OsRng = OsRng;
-    let private_key: RsaPrivateKey =
-        RsaPrivateKey::new(&mut rng, rsa_key_size).expect("failed to generate a key");
-    let public_key: RsaPublicKey = private_key.to_public_key();
-    let mut signed_data = private_key
-        .sign(PaddingScheme::new_pkcs1v15_sign_raw(), &sha_hasher_result)
-        .unwrap();
+    let result = <SHA256RSADigitalSignature as RSADigitalSignature>::digital_signature_rsa(rsa_key_size as u32, data_to_sign_slice);
+    let mut signed_data = result.signature;
     let capacity = signed_data.capacity();
     signed_data.reserve_exact(capacity);
     let result = SHARSADigitalSignatureResult {
-        public_key: CString::new(
-            public_key
-                .to_pkcs1_pem(rsa::pkcs8::LineEnding::LF)
-                .unwrap()
-                .to_string(),
-        )
-        .unwrap()
-        .into_raw(),
-        private_key: CString::new(
-            private_key
-                .to_pkcs8_pem(rsa::pkcs8::LineEnding::LF)
-                .unwrap()
-                .to_string(),
-        )
-        .unwrap()
-        .into_raw(),
+        public_key: CString::new(result.public_key).unwrap().into_raw(),
+        private_key: CString::new(result.private_key).unwrap().into_raw(),
         signature_raw_ptr: signed_data.as_mut_ptr(),
         length: signed_data.len(),
     };
@@ -132,29 +83,18 @@ pub extern "C" fn sha_512_rsa_digital_signature_verify(
         CStr::from_ptr(public_key)
     }
     .to_str()
-    .unwrap();
-    let data_to_verify_slice: &[u8] = unsafe {
+    .unwrap()
+    .to_string();
+    let data_to_verify_slice: Vec<u8> = unsafe {
         assert!(!data_to_verify.is_null());
         std::slice::from_raw_parts(data_to_verify, data_to_verify_length)
-    };
-    let signature_slice: &[u8] = unsafe {
+    }.to_vec();
+    let signature_slice: Vec<u8> = unsafe {
         assert!(!signature.is_null());
         std::slice::from_raw_parts(signature, signature_length)
-    };
-    let mut hasher = Sha3_512::new();
-    hasher.update(data_to_verify_slice);
-    let sha_hasher_result = hasher.finalize();
-    let public_key = RsaPublicKey::from_pkcs1_pem(public_key_string).unwrap();
-    let verified = public_key.verify(
-        PaddingScheme::new_pkcs1v15_sign_raw(),
-        &sha_hasher_result,
-        &signature_slice,
-    );
-    if verified.is_err() == false {
-        return true;
-    } else {
-        return false;
-    }
+    }.to_vec();
+    let result = <SHA512RSADigitalSignature as RSADigitalSignature>::verify_rsa(public_key_string, data_to_verify_slice, signature_slice);
+    result
 }
 
 #[no_mangle]
@@ -170,29 +110,18 @@ pub extern "C" fn sha_256_rsa_digital_signature_verify(
         CStr::from_ptr(public_key)
     }
     .to_str()
-    .unwrap();
-    let data_to_verify_slice: &[u8] = unsafe {
+    .unwrap()
+    .to_string();
+    let data_to_verify_slice: Vec<u8> = unsafe {
         assert!(!data_to_verify.is_null());
         std::slice::from_raw_parts(data_to_verify, data_to_verify_length)
-    };
-    let signature_slice: &[u8] = unsafe {
+    }.to_vec();
+    let signature_slice: Vec<u8> = unsafe {
         assert!(!signature.is_null());
         std::slice::from_raw_parts(signature, signature_length)
-    };
-    let mut hasher = Sha3_256::new();
-    hasher.update(data_to_verify_slice);
-    let sha_hasher_result = hasher.finalize();
-    let public_key = RsaPublicKey::from_pkcs1_pem(public_key_string).unwrap();
-    let verified = public_key.verify(
-        PaddingScheme::new_pkcs1v15_sign_raw(),
-        &sha_hasher_result,
-        &signature_slice,
-    );
-    if verified.is_err() == false {
-        return true;
-    } else {
-        return false;
-    }
+    }.to_vec();
+    let result = <SHA256RSADigitalSignature as RSADigitalSignature>::verify_rsa(public_key_string, data_to_verify_slice, signature_slice);
+    result
 }
 
 #[no_mangle]
