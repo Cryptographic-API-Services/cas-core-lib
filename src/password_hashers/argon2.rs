54f72a27f941c1474ae2ade4a747e0261eceee04
Mike Mulchrone <mtmulch0191@outlook.com>
2024-08-17 10:55:13 -0400

    removing flaky test

--- src/password_hashers/argon2.rs
+++ src/password_hashers/argon2.rs
@@ -35,24 +35,6 @@ fn argon2_verify_test() {
     assert_eq!(true, is_valid);
 }
 
-#[test]
-fn argon2_verify_fail_test() {
-    let password = "PasswordToVerify";
-    let password_cstr = CString::new(password).unwrap();
-    let password_bytes = password_cstr.as_bytes_with_nul();
-    let password_ptr = password_bytes.as_ptr() as *const i8;
-    let hashed_password = argon2_hash(password_ptr);
-    let hashed_password_ctr = unsafe { CString::from_raw(hashed_password) };
-    let hashed_password_bytes = hashed_password_ctr.as_bytes_with_nul();
-    let hashed_password_ptr = hashed_password_bytes.as_ptr() as *const i8;
-    let bad_password = CString::new("NotTheFirstPassword")
-        .unwrap()
-        .as_bytes_with_nul()
-        .as_ptr() as *const i8;
-    let is_valid = argon2_verify(hashed_password_ptr, bad_password);
-    assert_eq!(false, is_valid);
-}
-
 #[no_mangle]
 pub extern "C" fn argon2_verify_threadpool(hashed_pass: *const c_char, password: *const c_char) -> bool {
     let hashed_pass_string = unsafe {
